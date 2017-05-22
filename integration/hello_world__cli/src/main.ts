3c923a1ba500e8bb4cd65529833bed5ec369ee36
Miško Hevery <misko@hevery.com>
2017-05-22 11:20:28 -0700

    test: CLI HelloWorld uses null platform

--- integration/hello_world__cli/src/main.ts
+++ integration/hello_world__cli/src/main.ts
@@ -1,11 +1,11 @@
 import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { platform } from '@angular/core';
 
-import { AppModule } from './app/app.module';
+import { AppModuleNgFactory } from './$$_gendir/app/app.module.ngfactory';
 import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule);
+platform.bootstrapModuleFactory(AppModuleNgFactory);
